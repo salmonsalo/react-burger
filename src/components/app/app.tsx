36d2321fd100202140a31c5d4ed86d988986e7d6
sslo <salmohsalo@yandex.ru>
2024-06-13 19:03:22 +0500

    HomePage

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,36 +1,39 @@
-import React, { useEffect, useState } from "react";
-import appStyle from "./app.module.css";
-import AppHeader from "../app-header/app-header";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import { useGetIngredientsQuery } from "../../services/burger-ingedients/api";
+// import appStyle from "./app.module.css";
+// import AppHeader from "../app-header/app-header";
+// import { useGetIngredientsQuery } from "../../services/burger-ingedients/api";
+// import Home from "../../pages/home/home";
+
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../../pages/home/home";
 
 function App() {
-  const {
-    isLoading: loading,
-    error,
-    data: ingredients,
-  } = useGetIngredientsQuery({});
-  if (loading) {
-    return <h2>Загрузка...</h2>;
-  }
+  // const {
+  //   isLoading: loading,
+  //   error,
+  //   data: ingredients,
+  // } = useGetIngredientsQuery({});
+  // if (loading) {
+  //   return <h2>Загрузка...</h2>;
+  // }
 
-  if (!loading && error) {
-    return <h2>{`Ошибка: ${error}`}</h2>;
-  }
+  // if (!loading && error) {
+  //   return <h2>{`Ошибка: ${error}`}</h2>;
+  // }
 
-  if (!loading && ingredients.length === 0) {
-    return <h2>---</h2>;
-  }
+  // if (!loading && ingredients.length === 0) {
+  //   return <h2>---</h2>;
+  // }
 
   return (
-    <div className={appStyle.app}>
-      <AppHeader />
-      <main className={appStyle.main}>
-        <BurgerIngredients />
-        <BurgerConstructor />
-      </main>
-    </div>
+    <Routes>
+      <Route path="/home" element={<HomePage />}/>
+    </Routes>
+    // <div className={appStyle.app}>
+    //   <AppHeader />
+    //   <main className={appStyle.main}>
+    //   <Home />
+    //   </main>
+    // </div>
   );
 }
 
